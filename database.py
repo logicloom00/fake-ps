531b3455e7c7ef3800c56627c10cae7bd186aaa2
Alexey Gruzdev <a.gruzdev@itech.corp>
2024-01-31 15:02:58 +0300

    rename fix database issues

--- database.py
+++ database.py
@@ -5,6 +5,6 @@ import os
 
 
 DATABASE_URL = os.environ.get('DATABASE_URL') or "sqlite:///./fake_ps_db.db"
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False}, pool_size=20, max_overflow=0)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
--- main.py
+++ main.py
@@ -18,17 +18,20 @@ Base.metadata.create_all(bind=engine)
 @app.post("/operations")
 def create_operation(request: CreateOperationRequest):
     db = SessionLocal()
-    count_operation_created = db.query(Operation).count()
+    try:
+        count_operation_created = db.query(Operation).count()
 
-    operation = Operation(
-        operation_id=request.operation_id,
-        provider=request.provider,
-        operation_type=request.operation_type,
-        query_type=QueryType.INTERMEDIATE,
-    )
-    db.add(operation)
-    db.commit()
-    db.refresh(operation)
+        operation = Operation(
+            operation_id=request.operation_id,
+            provider=request.provider,
+            operation_type=request.operation_type,
+            query_type=QueryType.INTERMEDIATE,
+        )
+        db.add(operation)
+        db.commit()
+        db.refresh(operation)
+    finally:
+        db.close()
     
     response_data = {
         "query_type": operation.query_type,
@@ -43,43 +46,46 @@ def create_operation(request: CreateOperationRequest):
 @app.get("/operations/{operation_id}/check")
 def check_operation(operation_id: str):
     db = SessionLocal()
-    operation = db.query(Operation).filter(Operation.operation_id == operation_id).first()
-    
-    if not operation:
-        raise HTTPException(status_code=404, detail="Operation not found")
-    
-    count_operation_created = db.query(Operation).count()
-    
-    # Simulate status based on system state
-    system_current_state = get_system_state(count_operation_created)
-    if operation.status is None:
-        if system_current_state == SystemState.NORMAL:
-            if operation.created_at + timedelta(seconds=random.randint(2, 5)) < datetime.utcnow():
-                operation.status = OperationStatus.SUCCESS
-                operation.query_type = QueryType.FINAL
-            else:
+    try:
+        operation = db.query(Operation).filter(Operation.operation_id == operation_id).first()
+        
+        if not operation:
+            raise HTTPException(status_code=404, detail="Operation not found")
+        
+        count_operation_created = db.query(Operation).count()
+        
+        # Simulate status based on system state
+        system_current_state = get_system_state(count_operation_created)
+        if operation.status is None:
+            if system_current_state == SystemState.NORMAL:
+                if operation.created_at + timedelta(seconds=random.randint(2, 5)) < datetime.utcnow():
+                    operation.status = OperationStatus.SUCCESS
+                    operation.query_type = QueryType.FINAL
+                else:
+                    operation.status = None
+            elif system_current_state == SystemState.OVERLOADING:
+                if operation.created_at + timedelta(seconds=random.randint(15, 20)) < datetime.utcnow():
+                    operation.status = random.choice([OperationStatus.SUCCESS, OperationStatus.DECLINE])
+                    operation.query_type = QueryType.FINAL
+                else:
+                    operation.status = None
+            elif system_current_state == SystemState.FAILING:
+                if operation.created_at + timedelta(seconds=random.randint(1, 3)) < datetime.utcnow():
+                    operation.status = OperationStatus.FAILED
+                    operation.query_type = QueryType.FINAL
+                else:
+                    operation.status = None
+            elif system_current_state == SystemState.UNAVAILABLE:
                 operation.status = None
-        elif system_current_state == SystemState.OVERLOADING:
-            if operation.created_at + timedelta(seconds=random.randint(15, 20)) < datetime.utcnow():
-                operation.status = random.choice([OperationStatus.SUCCESS, OperationStatus.DECLINE])
-                operation.query_type = QueryType.FINAL
-            else:
-                operation.status = None
-        elif system_current_state == SystemState.FAILING:
-            if operation.created_at + timedelta(seconds=random.randint(1, 3)) < datetime.utcnow():
-                operation.status = OperationStatus.FAILED
-                operation.query_type = QueryType.FINAL
-            else:
-                operation.status = None
-        elif system_current_state == SystemState.UNAVAILABLE:
-            operation.status = None
-        elif system_current_state == SystemState.RECOVERING:
-            if operation.created_at + timedelta(seconds=random.randint(5, 10)) < datetime.utcnow():
-                operation.status = random.choice([OperationStatus.SUCCESS, OperationStatus.DECLINE])
-                operation.query_type = QueryType.FINAL
-            else:
-                operation.status = None
-        db.commit()
+            elif system_current_state == SystemState.RECOVERING:
+                if operation.created_at + timedelta(seconds=random.randint(5, 10)) < datetime.utcnow():
+                    operation.status = random.choice([OperationStatus.SUCCESS, OperationStatus.DECLINE])
+                    operation.query_type = QueryType.FINAL
+                else:
+                    operation.status = None
+            db.commit()
+    finally:
+        db.close()
     
     response_data = {
         "query_type": operation.query_type,
