db03f28a1085c2d6082f6b54d9693dc46bb905b3
Alexey Gruzdev <a.gruzdev@itech.corp>
2024-01-31 10:24:44 +0300

    add docker file

--- database.py
+++ database.py
@@ -1,9 +1,10 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
+import os
 
 
-DATABASE_URL = "sqlite:///./fake_ps_db.db"
+DATABASE_URL = os.environ.get('DATABASE_URL') or "sqlite:///./fake_ps_db.db"
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
